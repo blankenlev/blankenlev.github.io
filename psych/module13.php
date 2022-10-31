ecc53d088bae626d6955b9f8508405b01488abaf
Levi Blankenheim <levyguy88@gmail.com>
2022-10-31 16:34:41 -0500

    first Prototype

---
+++ psych/module13.php
@@ -0,0 +1,9 @@
+<?php
+include('navBar.php');
+?>
+<!--This is module 1 (open div class='content' tag)-->
+		<h1>Module 1</h1><br>
+		
+<?php
+include('pageFooter.html');
+?>
---
+++ psych/navBar.php
@@ -0,0 +1,27 @@
+<?php
+include('pageHeader.html');
+?>
+	<!--Inside head-->
+	</head>
+	<body>
+    <!--Inside body (no closing tag)-->
+		<h1 id='title'>Levi Blankenheim's Psych Term Project</h1>
+		<div class='sideNav'>
+			<button style='margin-top:0px' onclick="location.href='https://www.leviblankenheim.com/'" type='button'>Go Back</button><br>
+			<button onclick="location.href='index.php'" >Home</button><br>
+			<button onclick="location.href='https://www.leviblankenheim.com/psych/module1.php'" >Module 1</button><br>
+			<button onclick="location.href='https://www.leviblankenheim.com/psych/module2.php'" >Module 2</button><br>
+			<button onclick="location.href='https://www.leviblankenheim.com/psych/module3.php'" >Module 3</button><br>
+			<button onclick="location.href='https://www.leviblankenheim.com/psych/module4.php'" >Module 4</button><br>
+			<button onclick="location.href='https://www.leviblankenheim.com/psych/module5.php'" >Module 5</button><br>
+			<button onclick="location.href='https://www.leviblankenheim.com/psych/module6.php" >Module 6</button><br>
+			<button onclick="location.href='https://www.leviblankenheim.com/psych/module7.php'" >Module 7</button><br>
+			<button onclick="location.href='https://www.leviblankenheim.com/psych/module8.php'" >Module 8</button><br>
+			<button onclick="location.href='https://www.leviblankenheim.com/psych/module9.php'" >Module 9</button><br>
+			<button onclick="location.href='https://www.leviblankenheim.com/psych/module10.php'" >Module 10</button><br>
+			<button onclick="location.href='https://www.leviblankenheim.com/psych/module11.php'" >Module 11</button><br>
+			<button onclick="location.href='https://www.leviblankenheim.com/psych/module12.php'" >Module 12</button><br>
+			<button onclick="location.href='https://www.leviblankenheim.com/psych/module13.php'" >Module 13</button><br>
+			<button onclick="location.href='https://www.leviblankenheim.com/psych/module14.php'" >Module 14</button>
+		</div>
+		<div class='content'>
